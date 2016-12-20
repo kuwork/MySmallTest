645ea21e7428bec759fc75b1f4fdde096a82f6d0
Jerry <jerry535362936@126.com>
2016-12-20 15:13:48 +0800

    SDK升级到25.1.0 , 统一V4 V7 design 到外部定义版本

--- app.main/src/main/java/com/example/mysmall/app/main/ScrollingActivity.java
+++ app.main/src/main/java/com/example/mysmall/app/main/ScrollingActivity.java
@@ -5,7 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.TextureView;
 import android.view.View;
+import android.widget.VideoView;
 
 public class ScrollingActivity extends AppCompatActivity {
 
